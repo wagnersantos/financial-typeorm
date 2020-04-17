57ca7150dc39c7c4e44cd6598c6d90404ddacd56
wagnersantos <wagtvc@gmail.com>
2020-04-17 07:27:13 -0300

    remove unusable import

--- src/database/migrations/1587115292665-CreateTransations.ts
+++ src/database/migrations/1587115292665-CreateTransations.ts
@@ -1,9 +1,4 @@
-import {
-  MigrationInterface,
-  QueryRunner,
-  Table,
-  TableForeignKey,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateTransations1587115292665
   implements MigrationInterface {
