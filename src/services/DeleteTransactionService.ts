0fb0c6df3f599ea079402e436e1d512ea71ec6e1
wagnersantos <wagtvc@gmail.com>
2020-04-18 22:08:08 -0300

    added delete in transactions services

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: { id: string }): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!transaction) {
+      throw new AppError('ID not found!');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
